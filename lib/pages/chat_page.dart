c19e6e327417e9ded0c0dfd85aa2834ecb5ec7cf
Aahmedkamell <aahmed5kamell@gmail.com>
2026-08-16 01:50:48 +0300

    feat:

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scholar_chat/constants.dart';
+import 'package:scholar_chat/widgets/chat_bubble.dart';
 
 class ChatPage extends StatelessWidget {
   static String id = 'ChatPage';
@@ -17,6 +18,33 @@ class ChatPage extends StatelessWidget {
             Text('Chat', style: TextStyle(color: Colors.white)),
           ],
         ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return ChatBubble();
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Send Message',
+                suffixIcon: Icon(Icons.send, color: kPrimaryColor),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: BorderSide(color: kPrimaryColor),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
